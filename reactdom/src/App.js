410ccbf32d61cd875dc1e21c879a08fc8d0e672c
dingliwei <906289674@qq.com>
2018-04-19 17:43:41 +0800

    基础知识

--- reactdom/src/App.js
+++ reactdom/src/App.js
@@ -1,21 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+ class App extends Component{
+   render(){
+     const boss = '李云龙';
+     return (
+       <div>
+         <h2>独立团，团长{boss}</h2>
+         <一营 老大="张大彪"></一营>
+       </div>
+     )
+   }
+ }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-
-export default App;
+ class 一营 extends Component{
+   constructor(props){
+     super(props)
+     this.state = {
+       solders:['胡子','柱子','王根生']
+     }
+   }
+   render(){
+     return (
+       <div>
+         <h2>一营营长，{this.props.老大}</h2>
+         <ul>
+           {
+             this.state.solders.map(v=>{
+               return <li>{v}</li>
+             })
+           }
+         </ul>
+       </div>
+     )
+   }
+ }
+ export default  App
