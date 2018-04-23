0ff07b212ad7baf5d10de0a4f873a7a55df9e917
dingliwei <906289674@qq.com>
2018-04-23 19:09:53 +0800

    redux 谷歌调试

--- reactdom/src/index.js
+++ reactdom/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-import {createStore, applyMiddleware}  from 'redux'
+import {createStore, applyMiddleware, compose}  from 'redux'
 import thunk  from 'redux-thunk'
 import { counter, addGUN, removeGUN, addGUNAsync } from './index.redux'
 import App from './App'
 
 
-const store = createStore(counter,applyMiddleware(thunk))
+const store = createStore(counter,compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : () => { }
+))
 
 function render(){
     ReactDom.render(<App store={store} addGUNAsync={addGUNAsync} addGUN={addGUN} removeGUN={removeGUN} />, document.getElementById('root'))
