a7865f55c0d545f0dbaf477865f0cca57b5f4aa1
dingliwei <906289674@qq.com>
2018-04-23 20:48:41 +0800

    react-router

--- reactdom/src/index.js
+++ reactdom/src/index.js
@@ -5,6 +5,7 @@ import {createStore, applyMiddleware, compose}  from 'redux'
 import thunk  from 'redux-thunk'
 import { Provider} from  'react-redux'
 import { counter } from './index.redux'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import App from './App'
 
 
@@ -13,9 +14,35 @@ const store = createStore(counter,compose(
     window.devToolsExtension ? window.devToolsExtension() : () => { }
 ))
 
+function Erying(){
+    return <h2>二营</h2>
+}
+
+function San() {
+    return <h2>三营</h2>
+}
 ReactDom.render(
     (<Provider store={store}>
-        <App /> 
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>一营</Link>
+                    </li>
+                    <li>
+                        <Link to='/erying'>二营</Link>
+                    </li>
+                    <li>
+                        <Link to='/san'>三营</Link>
+                    </li>
+                </ul>
+                <Route path='/' exact component={App} />
+                <Route path='/erying' component={Erying} />
+                <Route path='/san' component={San} />
+            </div>
+            
+        </BrowserRouter>
+        
     </Provider>),
     document.getElementById('root')
 )
